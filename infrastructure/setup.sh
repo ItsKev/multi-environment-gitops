482e5758cda86cc13f2054dcdb0cdb72bb49ddb5
Kevin Huber <kevin.huber@hotmail.com>
2021-12-09 14:42:20 +0100

    Add overlays

--- infrastructure/setup.sh
+++ infrastructure/setup.sh
@@ -4,3 +4,5 @@ kind create cluster --config configs/kind-config.yaml
 
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install argo-cd -n argo --create-namespace argo/argo-cd --values configs/argo-cd-values.yaml
+
+kubectl apply -f k8s
