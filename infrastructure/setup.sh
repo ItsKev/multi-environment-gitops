adc73f3ddffd8f059e31ec11afa42c5735d7c9a8
Kevin Huber <kevin.huber@hotmail.com>
2021-12-09 18:25:37 +0100

    Update repo url

--- infrastructure/setup.sh
+++ infrastructure/setup.sh
@@ -4,5 +4,3 @@ kind create cluster --config configs/kind-config.yaml
 
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install argo-cd -n argo --create-namespace argo/argo-cd --values configs/argo-cd-values.yaml
-
-kubectl apply -f k8s
